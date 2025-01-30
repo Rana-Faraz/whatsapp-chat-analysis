e0175ec6385bb473787d59da70a4f8a3161f4e79
Rana Faraz <rana.faraz@logicon.com.pk>
2025-01-30 17:49:21 +0500

    test

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ heart_emojis = {
 
 @app.route('/hello')
 def hello():
-    return "<p>Hello, World!</p>"
+    return "Hello, World!"
 
 @app.route('/analyze', methods=['POST'])
 def analyze_chat():
